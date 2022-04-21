4b06e64bd3707c8b68412f0041c05055e7a9a6ac
AmmarSameer <63783730+AmmarSameer@users.noreply.github.com>
2022-04-21 04:48:01 +0300

    Update Exercise_4.py

--- Exercise_4.py
+++ Exercise_4.py
@@ -3,7 +3,6 @@ from numpy import fft
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-
 import matplotlib.pyplot as plt
 n = 100                    # Number of data points
 dx = 5.0                    # Sampling period (in meters)
@@ -22,19 +21,15 @@ fig, ax = plt.subplots()    #Plot the resulting signal
 ax.plot(x, fx)
 ax.set_xlabel('Time [f]')
 ax.set_ylabel('Signal amplitude');
-
 noise = np.random.normal(0, 1, 100)      # 0 is the mean of the normal distribution you are choosing from
                                          # 1 is the standard deviation of the normal distribution
                                          # 100 is the number of elements you get in array noise
 new_signal = fx + noise                  # adding noise to the original signal 
-
-
 fig, bx = plt.subplots()                 #Ploting the resulting signal 
 bx.plot(x, new_signal)
 bx.set_xlabel('Time [f]')
 bx.set_ylabel('Signal amplitude');
 bx.set_ylabel(r'$new_signal$', size = 'x-large')
-
 fxs=(fx - np.min(fx)) / (np.max(fx) - np.min(fx)) #Standardize of the original signal
 fxn=(new_signal - np.min(new_signal)) / (np.max(new_signal) - np.min(new_signal)) #Standardize the resulting signal
 print('\n fxs \n', fxs)
@@ -43,7 +38,6 @@ ax.plot(x, fxs)
 ax.set_xlabel('Time [f]')
 ax.set_ylabel('Signal amplitude');
 ax.set_xlabel(r'$Standardize the original signal$', size = 'x-large')
-
 fig, ax = plt.subplots()                 #Ploting the Standardize of the original signal 
 ax.plot(x, fxn)
 ax.set_xlabel('Time [f]')
